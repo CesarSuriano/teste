0042b603cec9e6ec588ccb5e0b6008cdffd7fd78
Cesar Suriano <cesaraugustokg6@hotmail.com>
2018-08-11 21:06:13 -0300

    foi

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var texto = document.getElementById("texto");
+var botao = document.getElementById("botao");
+
+function clicou () {
+
+    texto.style.display = "inline"
+    botao.style.display = "none"
+
+
+
+    audio.play()
+}
